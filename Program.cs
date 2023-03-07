7981caf7dd37f9c46c31585d9c4619f2871152b6
lehelkasza <lehel.kasa@ametek.com>
2023-03-07 13:03:55 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KL;
+
+namespace KL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            System.Console.WriteLine($"Szamolas: {ClassLib.Add(5, 2)}");
+        }
+    }
+}
